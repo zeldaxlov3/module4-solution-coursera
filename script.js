3143952d92d373aa9ffe3198d4b73155bc74077c
zeldaxlov3 <76008350+zeldaxlov3@users.noreply.github.com>
2021-02-16 05:12:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
 (function () {
-  var names = [
-    "Yaakov",
-    "John",
-    "Jen",
-    "Jason",
-    "Paul",
-    "Frank",
-    "Larry",
-    "Paula",
-    "Laura",
-    "Jim",
-  ];
-
-  for (var i = 0; i < names.length; i++) {
-    var firstLetter = names[i].charAt(0).toLowerCase();
-
-    if (firstLetter === "j") {
-      byeSpeaker.speak(names[i]);
-    } else {
-      helloSpeaker.speak(names[i]);
-    }
+   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+   for (var name in names) {
+      var firstLetter = names[name].charAt(0).toLowerCase();
+       if (firstLetter === 'j') {
+          byeSpeaker.speak(names[name]);
+  } else {
+    helloSpeaker.speak(names[name]);
   }
+}
+
 })();
